8a40bced1529ea2bf45206979c91fc5d0c87fa38
Alhourani, Noor <noor1989.general@gmail.com>
2025-09-15 20:57:51 +0300

    All files needed to run the front-end project are uploaded with a screen record represents a code walkthrough & its run on web

---
+++ NoorAlhourani front-end Task/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+function Fun1() {
+    document.getElementById("savebtn").style.backgroundColor = "burlywood";
+    document.body.style.backgroundImage = 'url("images/newb-ground.png")';
+}
+function Fun2() {
+    var x = document.getElementById("input").innerHTML;
+    x = alert(text = "hint: insert the required data inside the box");
+}
+
+// document.body.bodyElement.style.backgroundImage = 'url("image path")'; 
